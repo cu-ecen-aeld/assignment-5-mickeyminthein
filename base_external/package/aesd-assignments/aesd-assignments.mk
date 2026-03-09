b76bdab87f28312c83fea7218ba4f902c53cfdff
Mickey Thein <mickeyminthein@gmail.com>
2026-03-08 22:57:18 -0400

    update assignment 5 Part 2 modifications

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,8 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 16f12d1f1eb81b6c4cc33f9aedc1705661d1bf7e
+#AESD_ASSIGNMENTS_VERSION = 16f12d1f1eb81b6c4cc33f9aedc1705661d1bf7e
+AESD_ASSIGNMENTS_VERSION = 25d9fe4a94acfbde8f998cf8e7ca62e9d85f4341
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
@@ -16,11 +17,17 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	 # Install aesdsocket binary
+    $(INSTALL) -D $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+    # Install aesd socket startup script
+    $(INSTALL) -D $(@D)/server/startup_script.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
 	$(INSTALL) -D $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/writer
     $(INSTALL) -D $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/finder.sh
     $(INSTALL) -D $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
